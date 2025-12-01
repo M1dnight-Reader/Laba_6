ebf8bbb10d3a5a4954c61887c3b046c37856b94c
M1dnight-Reader <ga_rodionov1@mail.ru>
2025-12-01 16:53:08 +0300

    My_Update

--- src/HT1.m
+++ src/HT1.m
@@ -1,11 +1,6 @@
 clc
 close all
 
-k1 = 20;
-k2 = 45;
-T = 7;
-T1 = 0.025;
-t = 0.4;
 
 W1 = tf([t, 1], [T, 1])
 W2 = tf(k1, [T1, 1, 0])
@@ -17,12 +12,5 @@ W6 = series(W5_5, W3)
 
 Fx = feedback(W6, 1, -1)
 Fe = 1 - Fx
-bode(W6)
-figure
-nyquist(W6)
-figure
-step(Fx)
-figure
-impulse(Fx)
 
-
+draw_pictures;
---
+++ src/draw_pictures.m
@@ -0,0 +1,7 @@
+bode(W6)
+figure
+nyquist(W6)
+figure
+step(Fx)
+figure
+impulse(Fx)
